a718c83153d65b4797eb567e1133f72305cd81e4
josekinguvg <riv24376@uvg.edu.gt>
2026-04-01 11:15:16 -0600

    add navigation my post section

--- js/navigation.js
+++ js/navigation.js
@@ -3,21 +3,34 @@
 const homeSection = document.getElementById("homeSection");
 const createSection = document.getElementById("createSection");
 const detailSection = document.getElementById("detailSection");
+const myPostsSection = document.getElementById("myPostsSection");
 
 export function showHome() {
   homeSection.classList.remove("hidden");
   createSection.classList.add("hidden");
   detailSection.classList.add("hidden");
+  myPostsSection.classList.add("hidden");
+
 }
 
 export function showCreate() {
   homeSection.classList.add("hidden");
   createSection.classList.remove("hidden");
   detailSection.classList.add("hidden");
+  myPostsSection.classList.add("hidden");
+
 }
 
 export function showDetail() {
   homeSection.classList.add("hidden");
   createSection.classList.add("hidden");
   detailSection.classList.remove("hidden");
+  myPostsSection.classList.add("hidden");
+}
+
+export function showMyPosts() {
+  homeSection.classList.add("hidden");
+  createSection.classList.add("hidden");
+  detailSection.classList.add("hidden");
+  myPostsSection.classList.remove("hidden");
 }
