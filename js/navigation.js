e364244701a1f0e003cc21be378608a2e95d4791
josekinguvg <riv24376@uvg.edu.gt>
2026-04-01 10:32:11 -0600

    add navigation view detail section

--- js/navigation.js
+++ js/navigation.js
@@ -1,17 +1,23 @@
 // navigation.js — Manejo de navegación entre secciones
 
-import { loadPosts } from "./posts.js";
-
 const homeSection = document.getElementById("homeSection");
 const createSection = document.getElementById("createSection");
+const detailSection = document.getElementById("detailSection");
 
 export function showHome() {
   homeSection.classList.remove("hidden");
   createSection.classList.add("hidden");
-  loadPosts();
+  detailSection.classList.add("hidden");
 }
 
 export function showCreate() {
   homeSection.classList.add("hidden");
   createSection.classList.remove("hidden");
+  detailSection.classList.add("hidden");
+}
+
+export function showDetail() {
+  homeSection.classList.add("hidden");
+  createSection.classList.add("hidden");
+  detailSection.classList.remove("hidden");
 }
